1252d650d698cb9645e262f0f2ab608a9ca09bae
ErikSandquist <sandquistarn@gmail.com>
2023-11-08 17:17:45 +0100

    Added projects & fixes
    Optimized dot pattern more, added projects, started with mobile responsiveness and added array as local font

--- script.js
+++ script.js
@@ -9,12 +9,11 @@ html.setAttribute("data-theme", theme);
 localStorage.setItem("theme", theme);
 
 function themeToggle() {
-  if (theme == "dark") {
+  if (theme === "dark") {
     theme = "light";
   } else {
     theme = "dark";
   }
-  console.log(theme);
 
   localStorage.setItem("theme", theme);
   html.setAttribute("data-theme", theme);
@@ -33,8 +32,9 @@ let canvasRotation = 0;
 const angles = [0, 90, 180, 270]; // Which of the rotation the canvas can have to fake that the wave comes from all directions
 
 // Generate static dots
-let dotsX = 25;
-let dotsY = 25;
+let dotsX = 22;
+let dotsY = 22;
+let dotRadius = 3;
 let lineX = 0;
 let dotsTotal = dotsX * dotsY;
 const dots = [];
@@ -44,14 +44,14 @@ for (let i = 0; i < dotsTotal; i++) {
   let lineY = Math.floor(i / dotsY); // Get the line Y number by figuring out how many times you can divide with dotsY
 
   lineX++;
-  if (i % dotsX == 0) {
+  if (i % dotsX === 0) {
     lineX = 0;
   }
 
   const dot = {
     x: lineX * (canvas.width / (dotsX + 1)) + canvas.width / (dotsX + 1),
     y: lineY * (canvas.height / (dotsY + 1)) + canvas.height / (dotsY + 1),
-    radius: 1.5,
+    radius: dotRadius,
     color: "#ffffff",
   };
 
@@ -64,19 +64,19 @@ function drawDots() {
 
   let direction = (waveDirection * Math.PI) / 180; // Convert to radians
 
-  // Calculate the current position of the wave
-  let wavePosition = ((animationFrame / 5) % (dotsY + 20000)) - 20;
+  // The following line of code is made with assistance from ChatGPT and is calculating the position of the wave based on how many frames have passed
+  let wavePosition = ((animationFrame / 7) % (dotsY + 20000)) - 20;
 
   dots.forEach((dot) => {
     // Adjust the dot size based on the wave position
-    // The following 5 lines of code is made with ChatGPT from my own promts, it is a complex math calculation do get the posistion from the wave at an angle
+    // The following 4 lines of code is made with ChatGPT from my own promts, it is a complex math calculation do get the posistion from the wave at an angle
     let distance = Math.abs(
       dot.y -
         Math.tan(direction) * dot.x -
         wavePosition * (canvas.height / dotsY)
     );
 
-    dot.radius = 1.5 + Math.max(0, 6.9 - distance / 69); // Nice
+    dot.radius = dotRadius + Math.max(0, 13.37 - distance / 40);
 
     ctx.beginPath();
     ctx.arc(dot.x, dot.y, dot.radius, 0, Math.PI * 2);
@@ -91,14 +91,14 @@ setInterval(() => {
   let running = false;
   // Using dots.every to save performance, instead of forEach which isnt breakable every is on return false
   dots.every((dot) => {
-    if (dot.radius != 1.5) {
+    if (dot.radius != dotRadius) {
       running = true;
       return false;
     }
     return true;
   });
 
-  if (running == false) {
+  if (running === false) {
     // The following 2 lines of code is made with ChatGPT from my own promts, Could write it myself, just didnt want to spend to much time on it.
     waveDirection =
       Math.random() < 0.5 ? Math.random() * 45 : 315 + Math.random() * 45;
@@ -108,7 +108,7 @@ setInterval(() => {
     const canvasRotation = angles[randomIndex];
     canvas.style.transform = `rotate(${canvasRotation}deg)`;
   }
-}, 2000); // Change this to reset the wave faster and with a new angle
+}, 3000); // Change this to reset the wave faster and with a new angle
 
 // Animation loop
 function animate() {
